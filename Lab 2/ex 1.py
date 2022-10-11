1a30c44a64ef11f467064d78335a7681bb21d680
aanaolaru <79383164+aanaolaru@users.noreply.github.com>
2022-10-11 10:42:25 -0700

    ex 1 impr

--- Lab 2/ex 1.py
+++ Lab 2/ex 1.py
@@ -6,12 +6,11 @@ import arviz as az
 
 np.random.seed(1)
 
-
 a=[]
-x = stats.expon.rvs(0, 1/4, 10000)     #  random time
-y = stats.expon.rvs(0, 1/6, 10000)     #  random time
-b=x+y
-z=stats.binom.rvs(1, 0.4, size=10000) # = 1 cu 40% sanse si 0 cu 60% sanse 
+x = stats.expon.rvs(0, 1/4, 10000)     
+y = stats.expon.rvs(0, 1/6, 10000)     
+
+z=stats.binom.rvs(1, 0.4, size=10000) 
 
 for i in range(10000):
     if z[i]==1: #sub 40
@@ -20,11 +19,10 @@ for i in range(10000):
         a.append(y[i])  
     
 
-print(x)
-meann = np.mean(a)
-print(meann)
-stdd=np.std(a)
-print(stdd)
+mean = np.mean(a)
+print(mean)
+stdev=np.std(a)
+print(stdev)
 
 az.plot_posterior({'a':a}) # Afisarea aproximarii densitatii probabilitatilor, mediei, intervalului etc. variabilelor x,y,z
 plt.show() 
